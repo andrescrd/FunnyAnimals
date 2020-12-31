6e31a19e119aa55d1272c5898824583a1e7a2028
andrescrd <c.andresricardo@gmail.com>
2020-12-30 23:08:13 -0300

    adding box

---
+++ Source/FunnyAnimals/Public/Actors/FBox.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/FProjectile.h"
+#include "FBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUNNYANIMALS_API AFBox : public AFProjectile
+{
+	GENERATED_BODY()
+	
+};
